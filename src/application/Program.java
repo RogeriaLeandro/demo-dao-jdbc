cf028b632c05378780e7b13982ecb624a0e48eaf
RogeriaLeandro <rogeriaaraujoleandro@gmail.com>
2021-02-05 16:05:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 
 		Department departmentobj = new Department (1, "Books");
 		System.out.println(departmentobj);
+		
+		Seller sellerobj = new Seller (21, "Bob", "bob@gmail.com", new Date (), 3000.0, departmentobj);
+		System.out.println(sellerobj);
+		
 	}
 
 }
